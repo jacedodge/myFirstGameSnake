63b20bb34b4f63727256515f89c3b13b302bbe4b
jacedodge <77118738+jacedodge@users.noreply.github.com>
2021-08-20 13:31:47 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,47 @@
+import {update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, snakeIntersection, snakeBody} from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
+import {outsideGrid} from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
+
+//Game Timer and main setup
+function main (currentTime) {
+if (gameOver){
+    alert(snakeBody.length)
+   if (confirm('Press okay to restart')) {
+       window.location ='/'
+   }
+   return
+}
+
+window.requestAnimationFrame(main)
+const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000 
+if (secondsSinceLastRender < 1 / snakeSpeed) return
+lastRenderTime = currentTime
+
+update()
+draw()
+
+}
+
+window.requestAnimationFrame(main)
+
+//Function that handles positioning
+function update() {
+updateSnake()
+updateFood()
+checkDeath()
+}
+
+//Function that handles drawing the sprites
+function draw() {
+gameBoard.innerHTML = ''
+drawSnake(gameBoard)
+drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
